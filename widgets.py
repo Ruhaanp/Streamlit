0584db1eea59a67bab866e1ecf56ad7b94a6f2ac
Ruhaanp <ruhaanpruthi2@gmail.com>
2025-08-28 21:19:03 +0930

    Update widgets.py

--- widgets.py
+++ widgets.py
@@ -3,7 +3,7 @@ import streamlit as st
 name = st.text_input("Enter your name")
 birth_year = st.number_input("Your birth year",min_value=1900,max_value=2100,step=1)
 
-if name:
+if name and birth_year:
   current_year = 2025
   age = current_year - birth_year
   st.write(f"Hello {name}! Your age is {age}")
