e152cc852bb3ef0eb86448c41e822a5cafb6b940
Ruhaanp <ruhaanpruthi2@gmail.com>
2025-08-28 21:22:02 +0930

    Update widgets.py

--- widgets.py
+++ widgets.py
@@ -1,7 +1,11 @@
 import streamlit as st
 
-name = st.text_input("Enter your name")
-birth_year = st.number_input("Your birth year",min_value=1900,max_value=2100,step=1)
+col1,col2 = st.columns(2)
+
+with col1:
+  name = st.text_input("Enter your name")
+with col2: 
+  birth_year = st.number_input("Your birth year",min_value=1900,max_value=2100,step=1)
 
 if name and birth_year:
   current_year = 2025
