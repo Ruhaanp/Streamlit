567bb2737d3596aac1903a2f453e44776216e433
Ruhaanp <ruhaanpruthi2@gmail.com>
2025-09-02 21:48:44 +0930

    Add files via upload

---
+++ textimage (1).py
@@ -0,0 +1,21 @@
+import streamlit as st
+st.title("My file uploader playground")
+
+#upload homework(only text files allowed")
+hw_files = st.file_uploader("Upload your HW text files", type = ["txt","pdf"]
+                            , accept_multiple_files=True,key="homework")
+
+if hw_files:
+  st.write("Your HW files uploaded succesfully")
+  for i in hw_files:
+    st.write("----",i.name)
+    file_text = i.read()
+    st.text_area(file_text)
+#upload homework(only text files allowed")
+dr_files = st.file_uploader("Upload your DR files", type = ["png","jpg","jpeg"]
+                            , accept_multiple_files=True,key="drawing")
+
+if dr_files:
+  st.write("Your DR files uploaded succesfully")
+  for i in dr_files:
+    st.image(i,width=200)
